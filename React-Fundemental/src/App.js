78580b1dbc034f5bc92f593607350bc14893cdbe
HananMohammed <hananmohammed2468@gmail.com>
2022-03-16 16:01:35 +0200

    States

--- React-Fundemental/src/App.js
+++ React-Fundemental/src/App.js
@@ -1,15 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
-import {Great} from './components/Greet';
+import { Great } from './components/Greet';
 import Welcome from './components/Welcome';
+import Message from './components/Message';
 
 function App() {
-  return (
-    <div className="App">
-      <Great /> 
-      <Welcome /> 
-    </div>
-  );
+    return ( <
+        div className = "App" >
+        {
+            // <Great name="hanan" heroName="M7md"/ >
+            // <Great name="hanan2" heroName="M7md2">
+            //     <p> This is Children prop</p>
+            // </Great>
+            // <Great name="hanan3" heroName="M7md3" / >
+            // < Welcome  name="Clark" heroName="M7md"/ >
+            // < Welcome  name="Diana" heroName="M7md"/ >
+        }
+
+            <Message />
+        </div>
+    );
 }
 
 export default App;
--- React-Fundemental/src/components/Greet.js
+++ React-Fundemental/src/components/Greet.js
@@ -1,5 +1,12 @@
 import React from 'react';
 
-export const Great = () => <h1>Hello ...</h1>;  
+export const Great = (props) =>{
+    return(
+        <div>
+            <h1>Hello {props.name} a.k.a {props.heroName}...</h1>
+            {props.children}
+        </div>
+    )
+};  
 
 //export default Great;
