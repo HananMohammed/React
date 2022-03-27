3e4c24eeebfa7d933ae92c298dfca8e55e2f6352
HananMohammed <hananmohammed2468@gmail.com>
2022-03-27 16:21:29 +0200

    Higher Order Components Part2

--- React-Fundemental/src/components/ClickCounter.js
+++ React-Fundemental/src/components/ClickCounter.js
@@ -1,26 +1,14 @@
 import React, { Component } from 'react'
-
+import WithCounter from './WithCounter'
 class ClickCounter extends Component {
-    constructor(props) {
-      super(props)
-    
-      this.state = {
-         count: 0
-      }
-    }
-    incrementCount = ()=>{
-        this.setState(prevState=>{
-            return{ count: prevState.count+1 }
-        })
-    }
 render() {
-    const {count} = this.state
+    const {count, incrementCount} = this.props
     return (
     <div>
-        <button onClick={this.incrementCount}>Clicked {count} Times</button>
+        <button onClick={ incrementCount }>  Clicked {count} Times</button>
     </div>
     )
 }
 }
 
-export default ClickCounter
+export default WithCounter(ClickCounter)
