8061814a6b3350cc7a87dfc304a4a71c97db2677
hanan m7md <32298658+HananMohammed@users.noreply.github.com>
2022-04-13 06:25:51 +0200

    React Hooks Tutorial - 2 - useState Hook

--- React-Fundemental/src/components/ComponentF.js
+++ React-Fundemental/src/components/ComponentF.js
@@ -1,18 +1,16 @@
-import React, { Component } from 'react'
-import { UserConsumer } from './UserContext'
+import React, { Component } from "react";
+import { UserConsumer } from "./UserContext";
 
 class ComponentF extends Component {
   render() {
     return (
       <UserConsumer>
-          {
-              username=>{
-                return <div>Hello {username}</div>
-              }
-          }
+        {(username) => {
+          return <div> Hello {username} </div>;
+        }}
       </UserConsumer>
-    )
+    );
   }
 }
 
-export default ComponentF
+export default ComponentF;
--- React-Fundemental/src/components/UserContext.js
+++ React-Fundemental/src/components/UserContext.js
@@ -1,6 +1,6 @@
-import React from 'react'
-const UserContext = React.createContext('defaultValue');
+import React from "react";
+const UserContext = React.createContext("defaultValue");
 const UserProvider = UserContext.Provider;
 const UserConsumer = UserContext.Consumer;
-export { UserProvider, UserConsumer } 
+export { UserProvider, UserConsumer };
 export default UserContext;
