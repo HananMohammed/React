a8edd6fe7a73e03d617ed1f038c3a6b6163fb85b
hanan m7md <hanan.amin@roqay.com>
2022-05-05 18:03:56 +0200

    React Hooks Tutorial - 9 - Run effects only once

--- use-effect-hook/src/App.js
+++ use-effect-hook/src/App.js
@@ -1,11 +1,15 @@
 import ClassCounterOne from "./components/ClassCounterOne";
+import ClassMouse from "./components/ClassMouse";
 import HookCounterOne from "./components/HookCounterOne";
+import HookMouse from "./components/HookMouse";
 
 function App() {
   return (
     <div className="App">
       {/* <ClassCounterOne /> */}
-      <HookCounterOne />
+      {/* <HookCounterOne /> */}
+      {/* <ClassMouse /> */}
+      <HookMouse />
     </div>
   );
 }
